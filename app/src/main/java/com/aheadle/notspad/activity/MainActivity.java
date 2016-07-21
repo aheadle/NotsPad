615f00a6f411abfe4c1bcce1a15fe2e493823326
srift <srift@qq.com>
2016-07-21 16:15:21 +0800

    线程间通信测试

--- app/src/main/java/com/aheadle/notspad/activity/MainActivity.java
+++ app/src/main/java/com/aheadle/notspad/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.aheadle.notspad.activity;
+
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -15,18 +19,24 @@ import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.aheadle.notspad.dao.NotePadDao;
 import com.tagwire.notspad.R;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private NotePadDao dao;
     private Cursor cursor;
     private SimpleCursorAdapter adapter;
     private Spinner mSpinner;
 
     private Button contacts_btn;
+    //两个handler
+    private Handler mHandler;
+    private Handler nHandler;
+
     public static final String JSON_DATA = "...";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,8 +53,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(MainActivity.this,NotepadDetailActivity.class);
-                intent.putExtra("idKey",(int)id);
+                Intent intent = new Intent(MainActivity.this, NotepadDetailActivity.class);
+                intent.putExtra("idKey", (int) id);
                 startActivity(intent);
             }
         });
@@ -58,24 +68,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //数据源
         String[] mItems = getResources().getStringArray(R.array.spinnername);
         //绑定数据源
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.custom_spinner,mItems);
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.custom_spinner, mItems);
         //绑定
         mSpinner.setAdapter(spinnerAdapter);
 
         //想要测试一下gson
         //getJsonMsg();
-    }
+        //开两个线程，用来测试线程间的通讯
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                Log.e("msg", msg.obj + "");
+            }
+        };
 
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                nHandler = new Handler() {
+                    @Override
+                    public void handleMessage(Message msg) {
+                        super.handleMessage(msg);
+                        Log.e("msg", msg.obj + "");
+                        if (mHandler != null) {
+                            Message mMsg = mHandler.obtainMessage();
+                            mMsg.obj = mHandler.getLooper().getThread().getName() + "你好mHandler";
+                            mHandler.sendMessage(mMsg);
+                        }
+                    }
+                };
+
+
+                Looper.loop();
+            }
+        }).start();
+
+
+    }
 
 
     @Override
     protected void onResume() {
         super.onResume();
         reqeury();
+
     }
 
     private void reqeury() {
-        Log.d("TAG","onResume");
+        Log.d("TAG", "onResume");
         cursor = dao.query();
         adapter.changeCursor(cursor);
     }
@@ -84,21 +127,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(cursor!=null){
+        if (cursor != null) {
             cursor.close();
         }
+        nHandler.getLooper().quit();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (nHandler != null) {
+            Message msg = nHandler.obtainMessage();
+            msg.obj = nHandler.getLooper().getThread().getName() + "你好nHandler";
+            nHandler.sendMessage(msg);
+        }
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==R.id.noteAddId){
-            Intent intent = new Intent(this,NotepadAddActivity.class);
+        if (item.getItemId() == R.id.noteAddId) {
+            Intent intent = new Intent(this, NotepadAddActivity.class);
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
@@ -106,21 +160,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        getMenuInflater().inflate(R.menu.notepad_content,menu);
+        getMenuInflater().inflate(R.menu.notepad_content, menu);
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
         AdapterView.AdapterContextMenuInfo acm = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.noteDeleteId:
                 dao.delete(String.valueOf(acm.id));
                 reqeury();
                 break;
 
             case R.id.noteDetailId:
-                Intent intent = new Intent(this,NotepadDetailActivity.class);
-                intent.putExtra("idKey",(int)acm.id);
+                Intent intent = new Intent(this, NotepadDetailActivity.class);
+                intent.putExtra("idKey", (int) acm.id);
                 startActivity(intent);
                 break;
         }
@@ -129,9 +183,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.add_contacts:
-                startActivity(new Intent(MainActivity.this,ContactActivity.class));
+                startActivity(new Intent(MainActivity.this, ContactActivity.class));
                 break;
         }
     }
