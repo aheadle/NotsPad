445154da4b1d99d2311499d5740d5d3f2165ab51
srift <srift@qq.com>
2016-07-23 10:09:59 +0800

    leakcanary

---
+++ app/src/main/java/com/aheadle/notspad/application/MyApplication.java
@@ -0,0 +1,16 @@
+package com.aheadle.notspad.application;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by Jing on 16/7/23.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
